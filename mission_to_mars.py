91c31a2a1f5ee9f2aaf740f98e5c92148ff4019a
Akashalives7 <35281785+Akashalives7@users.noreply.github.com>
2018-10-02 21:13:08 -0700

    Add files via upload
    Downloaded Jupyter Notebook as Python

--- mission_to_mars.py
+++ mission_to_mars.py
@@ -3,255 +3,194 @@
 
 # In[117]:
 
-
 # Import BeautifulSoup for parsing and splinter to navigate site
 from splinter import Browser
 from bs4 import BeautifulSoup
 import os
 
-
 # In[118]:
-
+def scrape():
 
 # Load Chromedriver and prep the browser
-filepath = {'executable_path': './chromedriver'}
-browser = Browser('chrome', **filepath)
-
+    filepath = {'executable_path': './chromedriver'}
+    browser = Browser('chrome', **filepath)
 
 # In[119]:
 
-
 ## NASA MARS NEWS. Title and Paragraph.
-
 
 # In[120]:
 
-
 # Visit the NASA news URL to collect the latest News Title and Paragraph
-news_url = 'https://mars.nasa.gov/news/'
-browser.visit(news_url)
-
+    news_url = 'https://mars.nasa.gov/news/'
+    browser.visit(news_url)
 
 # In[121]:
 
-
 # Scrape page into BeautifulSoup
-news = BeautifulSoup(browser.html, 'html.parser')
-
+    news = BeautifulSoup(browser.html, 'html.parser')
 
 # In[122]:
 
-
 # Collect the latest News Title
-news_item = news.find('div', class_='content_title')
-
+    news_item = news.find('div', class_='content_title')
 
 # In[123]:
 
-
-news_title = news_item.text
-news_title
-
+    news_title = news_item.text
+    news_title
 
 # In[124]:
 
-
 # Collectthe latest Paragraph
-news_para = news.find('div', class_='article_teaser_body').text
-
+    news_para = news.find('div', class_='article_teaser_body').text
 
 # In[125]:
 
-
 # Display the Paragraph
-news_para
-
+    news_para
 
 # In[126]:
 
-
 # JPL MARS SPACE IMAGES.
-
 
 # In[127]:
 
-
 # Visit the JPL Mars URL to begin scraping to collect image
-image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-
+    image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 
 # In[128]:
 
-
-browser.visit(image_url)
-
+    browser.visit(image_url)
 
 # In[129]:
 
-
 # Click Full Image Button
-full_image_element = browser.find_by_id('full_image')
-full_image_element.click()
-
+    full_image_element = browser.find_by_id('full_image')
+    full_image_element.click()
 
 # In[130]:
 
-
 # Click More Info Button
-more_info_element = browser.find_link_by_partial_text('more info')
-more_info_element.click()
-
+    more_info_element = browser.find_link_by_partial_text('more info')
+    more_info_element.click()
 
 # In[131]:
 
-
 # Create new BeautifulSoup Object
-html = browser.html
-img_soup = BeautifulSoup(html, 'html.parser')
-
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
 
 # In[132]:
 
-
 # Display Image URL link for hi res image.
-img_url = img_soup.find('figure', class_='lede').find('img')['src']
-img_url
-
+    img_url = img_soup.find('figure', class_='lede').find('img')['src']
+    img_url
 
 # In[133]:
 
-
 # Adding the complete link to the hi res image listed above.
-base_url = f"https://www.jpl.nasa.gov{img_url}"
-
+    base_url = f"https://www.jpl.nasa.gov{img_url}"
 
 # In[134]:
 
-
 # Display the complete link of the image.
-base_url
-
+    base_url
 
 # In[135]:
-
 
 # MARS WEATHER
 # Visit the Mars Weather Twitter Account and Scrap the Latest Mars Weather Tweet.
 # tweepy not needed
 
-
 # In[136]:
 
-
 # get mars weather from mars twitter
-weather_url = 'https://twitter.com/marswxreport?lang=en'
-browser.visit(weather_url)
-
+    weather_url = 'https://twitter.com/marswxreport?lang=en'
+    browser.visit(weather_url)
 
 # In[137]:
 
-
-weather_html = browser.html
-weather_soup = BeautifulSoup(html3, 'html.parser') 
-
+    weather_html = browser.html
+    weather_soup = BeautifulSoup(html3, 'html.parser') 
 
 # In[138]:
 
-
-tweets = weather_soup.find_all('p', 'tweet-text')
-
+    tweets = weather_soup.find_all('p', 'tweet-text')
 
 # In[139]:
 
-
-weather_tweet = ''
-for tweet in tweets:
-    if tweet.text.split()[0] == 'Sol':
-        weather_tweet = tweet.text
-        print(tweet.text)
-        break
-
+    weather_tweet = ''
+    for tweet in tweets:
+        if tweet.text.split()[0] == 'Sol':
+            weather_tweet = tweet.text
+            print(tweet.text)
+            break
 
 # In[140]:
 
-
-weather_tweet
-
+    weather_tweet
 
 # In[141]:
-
 
 # MARS FACTS
 # Use Pandas to scrape the table containing facts about the planer including Diameter, Mass, etc.
 
-
 # In[142]:
 
-
 # Visit the website to gather Mars Facts.
-facts_url = 'http://space-facts.com/mars/'
-browser.visit(facts_url)
-
+    facts_url = 'http://space-facts.com/mars/'
+    browser.visit(facts_url)
 
 # In[143]:
 
-
 # Place Data Into a DataFrame,convert the Data to a HTML Table String.
-import pandas as pd
-fact_list = pd.read_html(facts_url)
-facts_df = fact_list[0]
-facts_table = facts_df.to_html(header=False, index=False)
-print(facts_table)  
-
+    import pandas as pd
+    fact_list = pd.read_html(facts_url)
+    facts_df = fact_list[0]
+    facts_table = facts_df.to_html(header=False, index=False)
+    print(facts_table)  
 
 # In[144]:
 
-
 # MARS HEMISPHERES Visit the USGS Astrogeology site to obtain high resolution images for each of Mars' hemispheres.
-
 
 # In[145]:
 
-
 # Go to USGS website
-usgs_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-browser.visit(usgs_url)
+    usgs_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(usgs_url)
 # URL Data and sends to BeautifulSoup
-usgs_html = browser.html                            
-usgs_soup = BeautifulSoup(usgs_html, 'html.parser') 
-hemisphere_image_urls = []                                                   # Creates empty list
+    usgs_html = browser.html                            
+    usgs_soup = BeautifulSoup(usgs_html, 'html.parser') 
+    hemisphere_image_urls = []                                                   
 
-products = usgs_soup.find('div', class_='result-list')                       # finds products
-hemispheres = products.find_all('div', class_='item')                        # finds hemispheres
+    products = usgs_soup.find('div', class_='result-list')                      
+    hemispheres = products.find_all('div', class_='item')                       
 # Loops gathering Data:
-for hemisphere in hemispheres:                                               # iterates through hemispheres
-    title = hemisphere.find('div', class_='description')
+    for hemisphere in hemispheres:                                              
+        title = hemisphere.find('div', class_='description')
     
-    title_text = title.a.text                                                # extracts cleaned title
-    title_text = title_text.replace(' Enhanced', '')
-    browser.click_link_by_partial_text(title_text)                           # (automated) click
+        title_text = title.a.text                                                
+        title_text = title_text.replace(' Enhanced', '')
+        browser.click_link_by_partial_text(title_text)                        
     
-    usgs_html = browser.html                                                 # acquires response from URL
-    usgs_soup = BeautifulSoup(usgs_html, 'html.parser')                                 # sends response to beautiful soup
+        usgs_html = browser.html                                                
+        usgs_soup = BeautifulSoup(usgs_html, 'html.parser')                              
 # Extracts Image URL.
-    image = usgs_soup.find('div', class_='downloads').find('ul').find('li')  # extracts image url
-    img_url = image.a['href']
+        image = usgs_soup.find('div', class_='downloads').find('ul').find('li')  
+        img_url = image.a['href']
 # Adds Dictionary to the List   
-    hemisphere_image_urls.append({'title': title_text, 'img_url': img_url})  # adds dictionary to list  
+        hemisphere_image_urls.append({'title': title_text, 'img_url': img_url})  
     
-    browser.click_link_by_partial_text('Back')                               # (automated) click back
-
+        browser.click_link_by_partial_text('Back')                             
 
 # In[146]:
 
-
 # Display the List of Dictionaries:
-hemisphere_image_urls                                                        # displays list of dictionaries
-
+    hemisphere_image_urls                                                       
 
 # In[147]:
 
-
 # Close Browser
-browser.quit()
-
+    browser.quit()
